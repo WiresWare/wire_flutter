698832a2b91ae806f397eeb8e83f878f0e9e69c4
Vladimir Cores (Minkin) <vladimir.minkin@gmail.com>
2021-12-26 06:47:31 +0300

    Attempt to use mobile database - sembast - for mobile

--- example/_shared/lib/todo/service/MobileDatabaseService.dart
+++ example/_shared/lib/todo/service/MobileDatabaseService.dart
@@ -2,6 +2,7 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
+import 'package:wire_example_shared/todo/data/vo/TodoVO.dart';
 
 import 'IDatabaseService.dart';
 
@@ -14,19 +15,23 @@ class MobileDatabaseService extends IDatabaseService {
 
   MobileDatabaseService();
 
-  var jsonList = <dynamic>[];
-
   @override
   Future<bool> init([String? key]) async {
     var dir = await getApplicationDocumentsDirectory();
     await dir.create(recursive: true);
     final dbPath = join(dir.path, 'application.db');
-    _db = await databaseFactoryIo.openDatabase(dbPath);
-    // final todoList = await store.record(key).get(_db);
-    // print('> MobileDatabaseService -> init: todoList = $todoList');
-    // jsonList.add({'id': '1', 'text': 'Read', 'note': 'books, journals, articles', 'completed': true});
-    // jsonList.add({'id': '2', 'text': 'Write', 'note': 'books, journals, articles', 'completed': false});
-    // jsonList.add({'id': '3', 'text': 'Travel', 'note': 'London, New York, Paris', 'completed': false});
+    DatabaseFactory dbFactory = databaseFactoryIo;
+    _db = await dbFactory.openDatabase(dbPath, version: 1, onVersionChanged: (db, oldVersion, newVersion) async {
+      if (oldVersion == 0) {
+        final store = intMapStoreFactory.store('store');
+        final id = key!.length;
+        store.record(id).add(_db, new TodoVO('1', 'Read', 'books, journals, articles', false).toJson());
+        store.record(id).add(_db, new TodoVO('2', 'Write', 'books, journals, articles', false).toJson());
+        store.record(id).add(_db, new TodoVO('3', 'Travel', 'London, New York, Paris', false).toJson());
+      }
+      print('> MobileDatabaseService -> onVersionChanged: $oldVersion | $newVersion');
+    });
+    print('> MobileDatabaseService -> init: key = $key | dbPath = $dbPath');
     return Future.value(true);
   }
 
@@ -36,8 +41,10 @@ class MobileDatabaseService extends IDatabaseService {
   }
 
   @override
-  dynamic retrieve(String key) {
-    return jsonList;
+  Future<dynamic> retrieve(String key) async {
+    final result = await store.record(key.length).get(_db);
+    print('> MobileDatabaseService -> retrieve: result = $result');
+    return result;
   }
 
   @override
